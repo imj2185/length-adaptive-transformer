fef570584bb829d683c8413037bf8a7c8b1e1263
imj2185 <dancingnum@gmail.com>
2022-05-02 10:55:19 -0400

    bash script for running glue

---
+++ glue_all.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+MODEL=$1
+# EXP_TYPE=$2
+# BENCHMARK=$3
+
+function standard_finetune () {
+    echo $1 $2 $3 "${4,,}"
+    #tmux send-keys -t "$1" "git checkout master" Enter
+    if  [$3 == "mobilebert"]
+    then
+        tmux send-keys -t "$1" "CUDA_VISIBLE_DEVICES=$2 python run_glue.py --model_name_or_path google/$3-uncased --task_name "${4,,}" --do_train --do_eval --data_dir glue/$4 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir glue_output/$4/standard" Enter
+    elif [$3 == "bert"]
+    then
+        tmux send-keys -t "$1" "CUDA_VISIBLE_DEVICES=$2 python run_glue.py --model_name_or_path $3-base-cased --task_name "${4,,}" --do_train --do_eval --data_dir glue/$4 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir glue_output/$4/standard" Enter
+    elif [$3 == "distilbert"]
+    then
+        tmux send-keys -t "$1" "CUDA_VISIBLE_DEVICES=$2 python run_glue.py --model_name_or_path $3-base-cased --task_name "${4,,}" --do_train --do_eval --data_dir glue/$4 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir glue_output/$4/standard" Enter
+    fi
+
+    #tmux send-keys -t "$1" "CUDA_VISIBLE_DEVICES=$2 python run_glue.py --model_name_or_path $3-base-cased --task_name sst-2 --do_train --do_eval --data_dir glue/SST-2 --max_seq_length 128 --per_device_train_batch_size 32 --per_device_eval_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir glue_output/SST-2/standard" Enter
+}
+
+cuda_id=0
+for bench in "CoLA" "MNLI" "QNLI" "QQP" "RTE" "SST-2" "STS-B" "WNLI" "MRPC"
+do
+    session=$bench
+    session+=_
+    session+=$MODEL
+
+    #echo $session
+    #echo $bench
+
+    tmux new-session -d -s "$session"
+    tmux send-keys -t "$session" "setenv PATH /home/mdl/cvl5361/softwares/a100/bin:/home/mdl/cvl5361/softwares/a100/condabin:/usr/local/cuda-11.4/bin:/home/grads/cvl5361/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:." Enter
+    tmux send-keys -t "$session" "bash" Enter
+    tmux send-keys -t "$session" "conda activate lat" Enter
+
+    #tmux send-keys -t "$session" "conda activate lat" Enter
+    standard_finetune $session $((cuda_id % 4)) $MODEL $bench
+    let "cuda_id+=1"
+
+done
+
+
+
+
